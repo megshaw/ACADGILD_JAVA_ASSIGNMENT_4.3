fdb11f58510d9f2e2896ef7f583cdcb16f8a58dc
meghana <meghana@Namsystech-Dev-01>
2017-04-10 11:28:21 +0530

    Java Assignment 4.3

---
+++ JavaAssignment 4.3/src/javaassignment/pkg4/pkg3/JavaAssignment43.java
@@ -0,0 +1,9 @@
+/*Assume that class A extends class B, which extends class C. Also all the three classes implement
+the method test(). How can a method in a class A invoke the test() method defined in class C
+(without creating a new instance of class C).*/
+
+
+
+/*Answer:
+b. super.test();
+*/
